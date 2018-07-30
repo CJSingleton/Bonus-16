cd87fa5d07a87d515d614b7987413535170e6dfc
Christopher <cjsing11@gmail.com>
2018-07-30 14:58:15 -0400

    First commit for bonus 16. Basic Specs.

---
+++ CSingleton_Bonus16/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSingleton_Bonus16
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }
+        }
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+    }
+}
---
+++ CSingleton_Bonus16/CarApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSingleton_Bonus16
+{
+    class CarApp
+    {
+
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();//new empty list
+
+            while (true)
+            {
+                Car temp = new Car(); //new empty car
+
+                Console.WriteLine("Please provide the make of the car.");
+                temp.Make = Console.ReadLine();
+
+                Console.WriteLine("Please provide the model for the car.");
+                temp.Model = Console.ReadLine();
+
+                Console.WriteLine("Please provide the year for the car.");
+                temp.Year = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please provide the price of the car.");
+                temp.Price = double.Parse(Console.ReadLine());
+
+                cars.Add(temp);// add the temp object to the 'cars' list.
+
+                Console.WriteLine("Do you want to add another car?");
+                string choice = Console.ReadLine();
+                if (choice == "n")
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("CAR DATABASE:");
+            foreach (Car e in cars)
+            {
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine($"Make: {e.Make}");
+                Console.WriteLine($"Model: {e.Model}");
+                Console.WriteLine($"Year: {e.Year}");
+                Console.WriteLine($"Price: {e.Price}");
+                Console.WriteLine("---------------------------------------");
+            }
+        }
+
+
+    }
+}
